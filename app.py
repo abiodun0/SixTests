e0e0cead5cb4277c1c091b598eccebbac20d8bd7
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2016-05-31 23:58:31 +0100

    Added in Python server

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request
-from firebase import firebase
+from pyfirebase import Firebase
 import os
 
-server_port = int(os.getenv('PORT')) or 5000
 app = Flask(__name__, template_folder='.', static_folder='build')
 
-firebase = firebase.FirebaseApplication(
-    'https://6ixtests.firebaseio.com',
-    None
-)
+firebase = Firebase('https://6ixtests.firebaseio.com')
+
 
 @app.route('/scorer/:exam_id', methods=['POST'])
 def scorer(exam_id):
     score = 0
     report_card = {'responses': {}}
     responses = request.get_json()
-    answers = firebase.get('/questions/{}/answers'.format(exam_id), None)
+    answers_ref = firebase.ref('questions/{}/answers'.format(exam_id))
+    answers = answers_ref.get()
     for q_id, correct_answer in answers:
         status = {'is_correct': False}
         if responses[q_id] == correct_answer:
@@ -24,14 +22,15 @@ def scorer(exam_id):
         status['correct_answer'] = correct_answer
         report_card['responses'][q_id] = status
     report_card['score'] = score
-    firebase.post('/submissions/{}/{}'.format(user_id, exam_id))
-    return redirect('/')
+    submissions_ref = firebase.ref('submissions/{}/{}'.format(user_id, exam_id))
+    try:
+        submissions_ref.push(report_card)
+        return redirect('/')
+    except Exception:
+        return "There was an error"
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def home(path):
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(port=server_port, debug=True)
